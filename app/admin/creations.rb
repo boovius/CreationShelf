0f546f892f2c3d1307a041ede1ad399cc8ed73c7
Joshua Book <joshua.book@gmail.com>
2020-04-29 17:09:43 -0400

    Add active admin resources for models.

---
+++ app/admin/creations.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Creation do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :summary, :creator_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :summary, :creator_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
