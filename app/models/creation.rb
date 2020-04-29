172b4c43377e08dd3feee9418581aa5bb1d2577e
Joshua Book <joshua.book@gmail.com>
2020-04-29 17:10:51 -0400

    Add header_photo relationship to Creation.

--- app/models/creation.rb
+++ app/models/creation.rb
@@ -5,4 +5,25 @@ class Creation < ApplicationRecord
 
   has_one :recipe
   has_many :photos, as: :holder
+  has_one :header_photo, class_name: 'Photo', as: :holder
+
+  def attributes
+    attrs = super.merge({
+      'photos': photos,
+      'header_photo': header_photo
+    })
+    attrs.delete('header_photo_id')
+    attrs
+  end
+
+  def create_header_photo(url:)
+    ActiveRecord::Base.transaction do
+      p = Photo.create!(url: url, holder: self)
+      self.update_attributes!(header_photo_id: p.id)
+    end
+  end
+
+  def set_header_photo(photo:)
+    self.update_attributes!(header_photo_id: photo.id)
+  end
 end
