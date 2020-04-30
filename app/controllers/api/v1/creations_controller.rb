29a4e720a2834c9e02e125c10c2ce7efbfc6dbdf
Joshua Book <joshua.book@gmail.com>
2020-04-30 17:30:25 -0400

    Endpoint for show Creation.

--- app/controllers/api/v1/creations_controller.rb
+++ app/controllers/api/v1/creations_controller.rb
@@ -4,4 +4,20 @@ class Api::V1::CreationsController < Api::ApiController
   def index
     render json: { data: Creation.all }, status: 200
   end
+
+  def show
+    id = creation_params.fetch(:id)
+    begin
+      c = Creation.find(id)
+      render json: { data: c }, status: 200
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { error: "No Creation found with id: #{id}" }, status: 404
+    end
+  end
+
+  private
+
+  def creation_params
+    params.permit(:id)
+  end
 end
